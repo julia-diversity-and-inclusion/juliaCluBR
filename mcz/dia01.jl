04317a652cfb28e838c94838ee8462a1eea9834e
graciellehigino <graciellehigino@hotmail.com>
2019-05-18 17:33:49 -0300

    mcz01 end

--- mcz/dia01.jl
+++ mcz/dia01.jl
@@ -105,3 +105,103 @@ if n_search in base
 else
     println(n_search, " Não encontrado")
 end
+
+
+---
+# Gráficos
+## Pacotes para DataFrames
+using Pkg
+#Pkg.installed() #lista os pacotes que você já tem e as correspondentes versões
+Pkg.update()
+Pkg.add([ #Instalar pacotes
+        "DataFrames",
+        "CSV",
+        "Statistics",
+        "Random",
+        "RDatasets"
+        ])
+using DataFrames, #Carregar
+        CSV,
+        Statistics,
+        Random,
+        RDatasets
+
+Pkg.add("Plots")
+
+## Criando um dataframe
+?DataFrames
+
+iris = dataset("datasets", "iris")
+
+tmp_iris = iris[iris[:Species] .== "setosa", :]
+
+deletecols!(tmp_iris, :PetalLength)
+
+mean(tmp_iris[:SepalLength])
+
+# Inserting a existing line
+insert!(df, 4, df2[1], :Arthur2)
+
+# Appending a existing row
+push!(df, df[1,1:4])
+
+#Criando conjuntos de dados:
+dados1 = ["Superman", "Batman", "Aquaman"]
+dados2 = ["Iroman", "Daredevil", "Hulk"]
+dados = DataFrame(DC = (dados1), Marvel = (dados2))
+heróis = dados
+
+dados3 = ["Primário", "Primário", "Primário", "Secundário", "Secundário", "Secundário"]
+dados4 = [15, 10, 12, 15, 18, 20]
+dados5 = [25, 24, 21, 27, 33, 40]
+planilha = DataFrame(Ambiente = (dados3), Riqueza = (dados4), Temperatura = (dados5))
+bichinhos = planilha
+
+Sith = ["Vader", "Palpatine", "Kylo", "Maul", "Dooku"]
+Jedi = ["Obi-Wan", "Yoda", "Luke", "Qui-Gon", "Anakin"]
+Lightsaber = DataFrame(Sith = (Sith), Jedi = (Jedi))
+
+#Exportando os dados:
+CSV.write("Bichinhos.csv", bichinhos, writeheader = true)
+CSV.write("Heróis.csv", heróis, writeheader = true)
+CSV.write("Star Wars.csv", Lightsaber, writeheader = true)
+
+#Importando os dados:
+CSV.read("Bichinhos.csv")
+CSV.read("Heróis.csv")
+CSV.read("Star Wars.csv")
+
+iris = dataset("datasets", "iris")
+flores = iris[4:5,]
+mean(flores.PetalWidth)
+mean(flores.PetalWidth[flores[:Species] .== "setosa", :])
+mean(flores.PetalWidth[flores[:Species] .== "virginica", :])
+
+
+dfg = DataFrame(pao=rand(list,5), queijo=rand(list,5), Arthur= rand(list,5))
+
+vetor = DataFrame(pao="batata",queijo="queijo",Arthur="Arthur")
+
+dfg1 = [dfg; vetor]
+
+dfg1 = dfg1[dfg1[3,].=="Bola",:] #Subset do dfg1, extraindo as linhas que, na coluna 3, têm o elemento "Bola"
+    # Semelhante a dfg1[dfg1.queijo.=="Bola"]
+describe(dfg1) #Descreve o dataframe
+dump(dfg) #lista os elementos do dataframe
+first(dfg,3) #Mostra as primeiras três linhas do df
+last(dfg,3) #Mostra as 3 últimas linhas do df
+unique(dfg[1]) #Retorna os valores únicos para a coluna 1 do df
+dfg2 = stack(dfg,2)
+dfg2 = unstack(dfg2,2)
+
+
+iris = dataset("datasets", "iris")
+iris2 = deletecols(iris,:PetalLength)
+iris2 = iris[iris.Species.=="setosa",:]
+length(iris2.PetalLength.<=quantile(iris2.PetalLength, 0.25))
+median(iris2.PetalLength)
+
+using Plots
+histogram(iris2.PetalLength)
+vline!([mean(iris2.PetalLength)],color="red")
+plot!(iris2.PetalLength)
